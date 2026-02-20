2392d36303cf8ffb0d5cd793077b306d93b0d366
Fernando nuñez <fernandonu@gmail.com>
2026-02-20 09:34:42 -0300

    nuevo login

--- login.php
+++ login.php
@@ -21,178 +21,192 @@ else
 <html>
 
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Sistema de Gestión Programa Sumar</title>
-<style type="text/css">
-<!--
-.Estilo1 {
-	font-family: Geneva, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	font-weight: bold;
-	color: #290478;/*290478;*/
-}
-.Estilo2 {
-	font-family: Geneva, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-}
-.Estilo4 {
-	font-family: Geneva, Arial, Helvetica, sans-serif;
-	font-size: 12px;
-	color: #000000;
-	font-weight: bold;
-}
-body {
-	background-image: url(imagenes/fondo3.jpg);
-	background-color: #A2A2A2;
-}
-.Estilo7 {font-size: 12px; font-weight: bold; font-family: Verdana, Arial, Helvetica, sans-serif; color: #FFFFFF; }
-.Estilo8 {
-	font-size: 16px;
-	font-weight: bold;
-	font-family: Arial, Helvetica, sans-serif;
-	color: #FFFFFF;
-}
-.Estilo10 {
-	font-family: Geneva, Arial, Helvetica, sans-serif;
-	font-size: 14px;
-	font-weight: bold;
-	color: #290478;
-}
-a:link {
-	text-decoration: none;
-	color: #333333;
-}
-a:visited {
-	text-decoration: none;
-	color: #333333;
-}
-a:hover {
-	text-decoration: none;
-	color: #FF3300;
-}
-a:active {
-	text-decoration: none;
-	color: #333333;
-}
--->
-</style>
-<link rel="icon" href="<? echo ((($_SERVER['HTTPS'])?"https":"http")."://".$_SERVER['HTTP_HOST']).$html_root; ?>/favicon.ico">
-<link REL='SHORTCUT ICON' HREF='<? echo ((($_SERVER['HTTPS'])?"https":"http")."://".$_SERVER['HTTP_HOST']).$html_root; ?>/favicon.ico'>
 
-<link type='text/css' href='<? echo $html_root; ?>/lib/estilos.css' REL='stylesheet'>
+<meta charset="UTF-8">
+<title>Programa SUMAR+</title>
+<link rel="icon" href="<? echo ((($_SERVER['HTTPS'])?"https":"https")."://".$_SERVER['HTTP_HOST']).$html_root; ?>/favicon.ico">
+<link REL='SHORTCUT ICON' HREF='<? echo ((($_SERVER['HTTPS'])?"https":"https")."://".$_SERVER['HTTP_HOST']).$html_root; ?>/favicon.ico'>
+
+
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+  background: #fff;
+
+  color: #fff;
+  font-family: Arial;
+  font-size: 12px;
+}
+
+.body{
+position: fixed; /* Mejor que absolute para fondos de pantalla completa */
+  top: 15px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  background-image: url(<?php echo $html_root;?>/imagenes/login/login<?php echo rand(1, 4);?>.jpg);
+  background-size: cover;       /* Cubre toda la pantalla sin deformar */
+  background-position: center;  /* Centra la imagen */
+  background-repeat: no-repeat; /* Evita que la imagen se duplique */
+  -webkit-filter: blur(0px);
+  z-index: 0;
+}
+
+.grad {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0.65) 100%);
+  z-index: 1;
+  opacity: 0.8; /* Aumentado un poco para mejorar legibilidad */
+}
+
+.header{
+  position: absolute;
+  top: calc(50% - 35px);
+  left: calc(50% - 555px);
+  z-index: 2;
+}
+
+.header div{
+  float: left;
+  color: #fff;
+  font-family: 'Exo', sans-serif;
+  font-size: 35px;
+  font-weight: 200;
+}
+
+.header div span{
+  color: #5379fa !important;
+}
+
+.login{
+  position: absolute;
+  top: calc(50% - 75px);
+  left: calc(50% - 50px);
+  height: 150px;
+  width: 350px;
+  padding: 10px;
+  z-index: 2;
+}
+
+.login input[type=text]{
+  width: 250px;
+  height: 30px;
+  background: transparent;
+  border: 1px solid rgba(255,255,255,0.6);
+  border-radius: 2px;
+  color: #fff;
+  font-family: 'Exo', sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  padding: 4px;
+}
+
+.login input[type=password]{
+  width: 250px;
+  height: 30px;
+  background: transparent;
+  border: 1px solid rgba(255,255,255,0.6);
+  border-radius: 2px;
+  color: #fff;
+  font-family: 'Exo', sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  padding: 4px;
+  margin-top: 10px;
+}
+
+.login input[type=button], .login input[type=submit]{
+  width: 260px;
+  height: 35px;
+  background: #fff;
+  border: 1px solid #fff;
+  cursor: pointer;
+  border-radius: 2px;
+  color: #a18d6c;
+  font-family: 'Exo', sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  padding: 6px;
+  margin-top: 10px;
+}
+
+.login input[type=button]:hover, .login input[type=submit]:hover{
+  opacity: 0.8;
+}
+
+.login input[type=button]:active, .login input[type=submit]:active{
+  opacity: 0.6;
+}
+
+.login input[type=text]:focus{
+  outline: none;
+  border: 1px solid rgba(255,255,255,0.9);
+}
+
+.login input[type=password]:focus{
+  outline: none;
+  border: 1px solid rgba(255,255,255,0.9);
+}
+
+.login input[type=button]:focus, .login input[type=submit]:focus{
+  outline: none;
+}
+
+::-webkit-input-placeholder{
+   color: rgba(255,255,255,0.6);
+}
+
+::-moz-input-placeholder{
+   color: rgba(255,255,255,0.6);
+}
+</style>
+
+<script src="./loginprefixfree.min.js"></script>
+
+
 </head>
 
-<body style="overflow:hidden;" onLoad="javascript: document.frm.username.focus();" topmargin="0" leftmargin="0" rightmargin="0" bottommargin="0" marginwidth="0" marginheight="0">
 <?if (!BROWSER_OK){?>
-  <script type="text/javascript">window.open ("https://www.google.com/chrome/browser/desktop/index.html","_blank")</script>
-  <script type="text/javascript">alert ("NAVEGADOR NO COMPATIBLE. Recomendamos Instalar Google Chrome.")</script>
+<script type="text/javascript">window.open ("https://www.google.com/chrome/browser/desktop/index.html","_blank")</script>
+<div class="alert alert-danger" align="center">NAVEGADOR NO COMPATIBLE. <a href="https://www.google.com/chrome/browser/desktop/index.html" target="_blank"> Recomendamos Instalar Google Chrome. </a></div>
 <?}?>
+
 <form action='index.php' method='post' name='frm'>
 <input type="hidden" name="resolucion_ancho" value="">
 <input type="hidden" name="resolucion_largo" value="">
-<div align="center">
-<br>
-<table width="586" border="0" align="center" cellspacing="10">
-  <tr>
-    <td><img src="imagenes/ministerio_login.jpg" alt="SISTEMA DE GESTION DEL PROGRAMA SUMAR" width="797" height="195"></td>
-  </tr>
-  <tr>
-    <td><table width="100%" border="0" cellspacing="0">
-        <tr>
-          <td width="62%" align="center"><table width="100" border="0" cellpadding="0" cellspacing="0">
-            <tr>
-              <td nowrap><table width="100%" border="0" cellpadding="0" cellspacing="0">
-                <tr>
-                  <td nowrap><table width="100%" border="0" cellpadding="5" cellspacing="0" bgcolor="#FFFFFF">
-                    <tr>
-                      <td nowrap><span class="Estilo1">Accesos directos:</span></td>
-                    </tr>
-                  </table></td>
-                </tr>
-                <tr>
-                  <td><img src="imagenes/somb.png" width="100%" height="11"></td>
-                </tr>
-              </table></td>
-            </tr>
-            <tr>
-              <td><table width="357" border="0" cellspacing="5">
-                  <tr>
-                    <td width="26" valign="middle" nowrap><img src="imagenes/flech.png" alt="&gt;" width="26" height="26" border="0"></td>
-                    <td width="312" valign="middle" nowrap=""><span class="Estilo4"><a href="http://www.salud.sanluis.gov.ar/">Sitio web Ministerio de Salud</a></span></td>
-                  </tr>
-                  <tr>
-                    <td valign="middle" nowrap><img src="imagenes/flech.png" alt="&gt;" width="26" height="26" border="0"></td>
-                    <td valign="middle" nowrap="" class="Estilo2"><a href="http://agenciasanluis.com/"><strong>Sitio web Agencia de Noticias</strong></a></td>
-                  </tr>
-                  <tr>
-                    <td valign="middle" nowrap><img src="imagenes/flech.png" alt="&gt;" width="26" height="26" border="0"></td>
-                    <td valign="middle" nowrap="" class="Estilo2"><a href="http://www.capacitacionsumar.msal.gov.ar/"><strong>Plataforma de capacitacion</strong></a></td>
-                  </tr>
-                  
-              </table></td>
-            </tr>
-          </table></td>
-          <td width="38%" align="center"><p>&nbsp;</p>
-            <table width="100" border="0" cellpadding="0" cellspacing="0">
-            <tr>
-              <td width="1" align="right"><img src="imagenes/caja1.png" width="18" height="174"></td>
-              <td background="imagenes/caja2.png"><form method="POST">
-                <table width="100" border="0" cellspacing="7">
-                  <tr>
-                    <td colspan="2"><div align="center" class="Estilo8">Ingreso al Sistema</div></td>
-                    </tr>
-                  <tr>
-                    <td><div align="right" class="Estilo7">Usuario:</div></td>
-                    <td><span style="text-align: right">
-                      <INPUT name=username AUTOCOMPLETE="off" style="border-style: solid; border-width: 1px" size="18" tabindex="1">
-                    </span></td>
-                  </tr>
-                  <tr>
-                    <td><div align="right" class="Estilo7">Contrase&ntilde;a:</div></td>
-                    <td><span style="text-align: right">
-                      <INPUT type=password name=password AUTOCOMPLETE="off" style="border-style: solid; border-width: 1px" size="18" tabindex="2">
-                    </span></td>
-                  </tr>
-                  <tr>
-                    <td colspan="2" align="center"><label>
-                      <div align="center"><span style="text-align: right">
-                          <INPUT name="loginform" type="submit" class="Estilo10"value="Ingresar">
-                        </span></div>
-                    </label></td>
-                  </tr>
-                </table>
-                </form>                </td>
-              <td width="1" align="left"><img src="imagenes/caja3.png" width="18" height="174"></td>
-            </tr>
-          </table>
-</td>
-        </tr>
-      </table></td>
-  </tr>
-  <tr>
-    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
-      <tr>
-        <td bgcolor="#05009F"><table width="100" border="0" align="center" cellpadding="0" cellspacing="10">
-          <tr>
-            <td nowrap><span class="Estilo7"><?php echo (date("Y") != "2014") ? "2014-".date("Y") : "2014"; ?> <strong>&copy;</strong> Copyright MINISTERIO DE SALUD - PROVINCIA DE SAN LUIS</span></td>
-          </tr>
-        </table></td>
-      </tr>
-      <tr>
-        <td><img src="imagenes/somb.png" width="100%" height="11"></td>
-      </tr>
-    </table>
-      </td>
-  </tr>
-</table>
-</div>
-<script>
-//guardamos la resolucion de la pantalla del usuario en los hiddens para despues recuperarlas
-//y guardarlas en las variable de sesion $_ses_user
-document.all.resolucion_ancho.value=screen.width;
-document.all.resolucion_largo.value=screen.height;
+	  
+      		    <form method="POST">
+                <div class="body"></div>
+                <div class="grad"></div>
+                <div class="header">
+                  <div> SUMAR+ <span> San Luis</span></div>
+                </div>
+                <br>
+                <div class="login">
+                    <input type="text" placeholder="Nombre de Usuario" name="username" pattern="[a-z0-9_]+" title="Letras minúsculas, números y guion bajo (_) sin espacios">                    <input type="password" placeholder="Clave" name="password"><br>
+                    <input type="submit" name="loginform" value="Iniciar Sesi&oacute;n">
+                    <br>
+                    <br>
+                    <a href="http://sisweb.msal.gov.ar/consultapyp/index.php" target="_blank" title="Consultas a PUCO. Usuario y contrase&ntilde;a: 'consulta'"> Consultas a PUCO </a>
+                    <br>
+                    <br>
+                    <a href="https://salud.sanluis.gov.ar/" target="_blank" title="Sitio web Ministerio de Salud"> Sitio web Ministerio de Salud </a>
+                </div>               
+              </form>
 
+<script>
+  //guardamos la resolucion de la pantalla del usuario en los hiddens para despues recuperarlas
+  //y guardarlas en las variable de sesion $_ses_user
+  document.all.resolucion_ancho.value=screen.width;
+  document.all.resolucion_largo.value=screen.height;
 </script>
-</body>
+<script type="text/javascript" src="<?php echo $html_root;?>/lib/jquery/jquery-1.11.1.min.js"></script>
 </form>
+</body>
 </html>
