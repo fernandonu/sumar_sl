1be77771ed0317daed1ee6ee4e26a16626911d7a
Fernando nuñez <fernandonu@gmail.com>
2026-04-21 15:04:59 -0300

    arreglo

--- modulos/inscripcion/ins_admin_old.php
+++ modulos/inscripcion/ins_admin_old.php
@@ -210,7 +210,8 @@ if ($_POST['guardar_editar']=="Guardar"){
                        
              where id_beneficiarios=".$id_planilla;
   		 }
-  	
+  
+  echo "responsable: ".$id_categoria. "---". $estado_envio;
 	if (($id_categoria=='1') && ($estado_envio== 'n'))  {
    			$query = "update uad.beneficiarios set 
              cuie_ea='$cuie', nombre_benef=upper('$nombre'),
@@ -335,7 +336,7 @@ if(($responsable =='MADRE') && ($estado_envio== 'e')){
                        
              where id_beneficiarios=".$id_planilla;  		 
   		 }
-  		 if ((($id_categoria=='7')||($id_categoria=='8'))&& ($estado_envio=='e'))  {
+  		 if ((($id_categoria=='4')||($id_categoria=='7')||($id_categoria=='8'))&& ($estado_envio=='e'))  {
    			$query = "update uad.beneficiarios set 
              cuie_ea='$cuie', nombre_benef=upper('$nombre'),
              apellido_benef=upper('$apellido'),aficuil='$aficuil',
